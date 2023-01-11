55a491db7ac3c09d7bc571c2102932814f116b1d
Pranav Tondgaonkar <pranavrelds@gmail.com>
2023-01-12 03:32:03 +0530

    changed logging to logger

--- config/logger.py
+++ config/logger.py
@@ -8,7 +8,7 @@ os.makedirs(LOG_DIR, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
 
-logging.basicConfig(filename=LOG_FILE_PATH,
+logger.basicConfig(filename=LOG_FILE_PATH,
                     filemode="w",
                     format='[%(asctime)s] \t%(levelname)s \t%(lineno)d \t%(filename)s \t%(funcName)s() \t%(message)s',
                     level=logging.INFO
